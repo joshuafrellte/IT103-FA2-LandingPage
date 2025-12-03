ebf8f60429a71407a807176fcc33ad33b1b8f3ef
unknown <joshuafrellt@gmail.com>
2025-12-03 17:54:37 +0800

    feat: basic responsive layout

---
+++ script.js
@@ -0,0 +1,9 @@
+const allSections = document.getElementsByTagName('section')
+
+// setSectionZIndex()
+
+// function setSectionZIndex() {
+//     for (let i=allSections.length-1; i>=0; i--) {
+//         allSections[i].style.zIndex = i;
+//     }
+// }
